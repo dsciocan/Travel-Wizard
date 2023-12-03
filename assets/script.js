72941980fd70b0e225e9b9733e6ea91f8d69ed4f
Daniela <daniela.ciocan137@gmail.com>
2023-12-03 23:22:59 +0000

    Start location autocomplete function

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,54 @@
 //Date pickcer code
-
-let endDate = document.getElementById('endDate')
-
-startDate.addEventListener('change',(e)=>{
-  let startDateVal = e.target.value
-  document.getElementById('startDateSelected').innerText = startDateVal
+let startDate = $('#startDate')
+let endDate = $('#endDate')
+let startDateVal;
+let endDateVal;
+var today=dayjs().format("YYYY-MM-DD");
+startDate.on('change',(e)=>{
+    var dateToday = dayjs().toDate();
+    var selectedDate = new Date(e.target.value)
+  if(selectedDate >= dateToday) {
+  startDateVal = e.target.value;
+} else {
+    e.target.value = today;
+    startDateVal = e.target.value;
+    $("#EndDateError").text("Departure date cannot be before today")
+}
 })
 
-endDate.addEventListener('change',(e)=>{
-  let endDateVal = e.target.value
-  document.getElementById('endDateSelected').innerText = endDateVal
+endDate.on('change',(e)=>{
+  if(e.target.value > startDateVal) {
+  endDateVal = e.target.value;
+  $("#EndDateError").text("")
+  } else {
+  e.target.value = startDateVal;
+  $("#EndDateError").text("Return date cannot be before departure date")
+  }
 }) 
 
+var fromCode;
+var toCode;
+
+$(function () {
+    var autocomplete = [];
+    var names = $.map(airports, function(o) { return o["name"]; }) 
+    var codes = $.map(airports, function(o) { return o["iata"]; })
+    var cities = $.map(airports, function(o) { return o["city"]; })
+    var searchCodes = [];
+    for(i=0;i<names.length;i++) {
+        if(names[i].search("Station") == -1) {
+            if(cities[i].search($("#from").val()) != -1 || codes[i].search($("#from").val()) != -1) {
+            autocomplete.push(String(cities[i] + " " + codes[i] + " - " + names[i]));
+            searchCodes.push(codes[i])
+        }
+    }
+    }
+    var fromValue;
+    $("#from").autocomplete({
+    source: autocomplete,
+
+    });
+
+ 
+
+})
