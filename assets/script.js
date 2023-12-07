98973f1fa13cd8fce57be7618763d33cc478be2a
emeritolopes <emerito.lopes@gmail.com>
2023-12-07 22:21:56 +0000

    deleted old comments dark theme

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,3 @@
-// // target the button using the data attribute we added earlier
-// const button = document.querySelector("[data-theme-toggle]");
-
-// button.addEventListener("click", () => {
-//   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-
-//   // update the button text
-//   const newCta = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
-//   button.innerText = newCta;  
-
-//   // use an aria-label if you are omitting text on the button
-//   // and using sun/moon icons, for example
-//   button.setAttribute("aria-label", newCta);
-
-//   // update theme attribute on HTML to switch theme in CSS
-//   document.querySelector("html").setAttribute("data-theme", newTheme);
-
-//   // update in local storage
-//   localStorage.setItem("theme", newTheme);
-
-//   // update the currentThemeSetting in memory
 
 //Date pickcer code
 let startDate = $('#startDate')
